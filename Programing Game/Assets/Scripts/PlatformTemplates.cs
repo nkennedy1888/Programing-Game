1223692b681ed996c8fc9c3ce22c1d541ec98bd8
nkennedy1888 <91894687+nkennedy1888@users.noreply.github.com>
2021-11-05 11:28:51 -0500

    Add Level gen

---
+++ Programing Game/Assets/Scripts/PlatformTemplates.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformTemplates : MonoBehaviour
+{
+    //Array of platform parts with index defining color
+    public GameObject[] left;
+    public GameObject[] middle;
+    public GameObject[] right;
+
+}
