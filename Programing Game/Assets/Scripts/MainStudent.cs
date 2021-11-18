fc63b1c57f4e23322761f6f6bf8d8e43d1dee65c
N D <48074458+SrMauzer@users.noreply.github.com>
2021-11-17 23:29:14 -0600

    Implemented some keyboard shortcuts and introduced some UI elements
    + Added UI for password change, functionality not complete
    + Added tab function script, currently only used in login screen

--- Programing Game/Assets/Scripts/MainStudent.cs
+++ Programing Game/Assets/Scripts/MainStudent.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,19 +8,32 @@ using UnityEngine.SceneManagement;
 public class MainStudent : MonoBehaviour
 {
 
-    public GameObject settings_Parent, account_Parent;
+    public GameObject settings_Parent, account_Parent, changePassParent;
 
+    string newPass, confPass;
+    private string path = "Assets/SaveData/users.txt";
     // Start is called before the first frame update
     void Start()
     {
         account_Parent.SetActive(false);
         settings_Parent.SetActive(false);
+        changePassParent.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void GetNewPass(string temp)
+    {
+        newPass = temp;
+    }
+
+    public void GetConfPass(string temp)
+    {
+        confPass = temp;
     }
 
     public void ToggleSettingsVis()
@@ -34,8 +48,53 @@ public class MainStudent : MonoBehaviour
 
     }
 
+    public void TogglePassChangeVis()
+    {
+        changePassParent.SetActive(!changePassParent.activeSelf);
+    }
+
     public void NavigateToBattle()
     {
         SceneManager.LoadScene("Battle");
     }
+
+    void changePassword()
+    {
+        if (!newPass.Equals(confPass))
+        {
+            //"Passwords must match" dialogue
+            return;
+        }
+        
+
+    }
+
+    public string GetUsername()
+    {
+        return PlayerPrefs.GetString("username");
+    }
+
+    string GetPasswordFromFile()
+    {
+        
+        string pass = "";
+        string line;
+
+        StreamReader reader = new StreamReader(path);
+
+        while ((line = reader.ReadLine()) != null && line != "")
+        {
+            if (line.StartsWith("name") && line.Substring(line.IndexOf(":") + 2).Equals(GetUsername()))
+            {
+                line = reader.ReadLine();
+                pass = line.Substring(line.IndexOf(":") + 2);
+                break;
+            }
+        }
+        reader.Close();
+        return pass;
+    }
+
+    //Implement method to change password.
+
 }
