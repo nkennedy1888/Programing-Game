5383d589452c35ab39ca1dd3025734b16a3ee176
nkennedy1888 <91894687+nkennedy1888@users.noreply.github.com>
2021-11-28 13:31:30 -0600

    battle stuff

--- Programing Game/Assets/Scripts/Battle.cs
+++ Programing Game/Assets/Scripts/Battle.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Cinemachine;
 
 public class Battle : MonoBehaviour
@@ -10,20 +11,37 @@ public class Battle : MonoBehaviour
     private GameObject player;
     public GameObject battleUI;
     public GameObject battleCam;
+    public int pMax;
+    public int eMax;
+    private int pAttk;
+    private int eAttk;
+    private int pCurr;
+    private int eCurr;
+    public Text pHealth;
+    public Text eHealth;
+    public Slider pBar;
+    public Slider eBar;
 
     // Start is called before the first frame update
     void Start()
     {
         isBattle = false;
+        pCurr = pMax;
+        eCurr = eMax;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+
+        pBar.value = CalculateStat(pCurr, pMax);
+        eBar.value = CalculateStat(eCurr, eMax);
+        pHealth.text = pCurr + " / " + pMax;
+        eHealth.text = eCurr + " / " + eMax;
     }
 
-    public void Fight() 
+    public void Fight()
     {
         ScreenFade sf = GameObject.FindGameObjectWithTag("Fader").GetComponent<ScreenFade>();
         isBattle = true;
@@ -35,4 +53,12 @@ public class Battle : MonoBehaviour
         StartCoroutine(sf.FadeToClear());
 
     }
+    float CalculateStat(int currstat, int maxstat)
+    {
+        float num = currstat / maxstat;
+
+        return num;
+
+    }
+
 }
